7f217e6e952fd3a8e0235cfa0c246e6e29f9c30c
Josh Zimmerman <JoshuaZimmerman@quickenloans.com>
2018-10-12 14:57:15 -0400

    Add project files.

---
+++ DevBuild.Buy_A_Vowel_JoshuaZimmerman/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBuild.Buy_A_Vowel_JoshuaZimmerman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
